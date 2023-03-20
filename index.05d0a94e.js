1df90b50a6d4fa94c8c22c5a4329a863120afd42
stark1269 <112755303+stark1269@users.noreply.github.com>
2023-03-20 21:18:54 +0000

    Deploying to gh-pages from @ stark1269/BarberShop@447ee700070aa8bc4caca5feb8e045c542966df2 🚀

---
+++ index.05d0a94e.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".nav-icon"),n=document.querySelector(".mob-menu");e.addEventListener("click",(function(){e.classList.toggle("open"),n.classList.toggle("mob-menu-open")}))}();
+//# sourceMappingURL=index.05d0a94e.js.map
