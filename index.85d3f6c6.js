3c1d96f88fc5a18d32f75fbcdb314c4a75bf0bc5
stark1269 <112755303+stark1269@users.noreply.github.com>
2023-03-21 01:42:47 +0000

    Deploying to gh-pages from @ stark1269/BarberShop@1a8a5cae632cc6f90d142cc5ee263b4042a458ab 🚀

--- index.85d3f6c6.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".nav-icon"),o=document.querySelector(".mob-menu");e.addEventListener("click",(()=>{e.classList.toggle("open"),o.classList.toggle("mob-menu-open")}));
-//# sourceMappingURL=index.85d3f6c6.js.map
