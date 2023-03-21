1a8a5cae632cc6f90d142cc5ee263b4042a458ab
stark1269 <stark1269@gmail.com>
2023-03-21 03:42:20 +0200

    add blog section

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,20 @@ mobMenuBtn.addEventListener('click', () => {
   mobMenuBtn.classList.toggle('open');
   mobMenu.classList.toggle('mob-menu-open');
 });
+
+// Клик по фото в секции "блог"
+
+const blogCard = document.querySelector('.blog-card');
+const frontCard = document.querySelector('.front-card');
+const backCard = document.querySelector('.back-card');
+
+const blogTextFirst = document.querySelector('.blog-text-first');
+const blogTextSecond = document.querySelector('.blog-text-second');
+
+
+blogCard.addEventListener('click', () => {
+  frontCard.classList.toggle('click-front');
+  backCard.classList.toggle('click-back');
+  blogTextFirst.classList.toggle('slide-first');
+  blogTextSecond.classList.toggle('slide-second');
+});
